3b338387a72f8dfb980f85f8b93dc062938f38b9
Sumeet Padavala <sumeet.padavala@gmail.com>
2023-05-27 23:02:38 -0700

    Remove custom slice logic

--- project6/LCS.java
+++ project6/LCS.java
@@ -1,71 +1,59 @@
 package project6;
 
-import java.util.Arrays;
-
-class Slice {
-  private String str;
-  private int start;
-  private int end;
-
-  Slice(String str) {
-    this.str = str;
-    this.start = 0;
-    this.end = str.length();
+class StringUtils {
+  static boolean contains(String s, char c) {
+    return s.contains(String.valueOf(c));
   }
 
-  private Slice(String str, int start, int end) {
-    assert end <= str.length();
-    assert start >= 0;
-    this.str = str;
-    this.start = start;
-    this.end = end;
+  static String reversed(String s) {
+    return new StringBuilder(s).reverse().toString();
+  }
+}
+
+class Debug {
+  static boolean DEBUG = false;
+
+  static void printHeader(String a, String b) {
+    if (!DEBUG)
+      return;
+    Debug.printf("\nTable for '%s' x '%s'\n", a, b);
+    System.out.print("  ");
+    for (int i = 0; i < b.length(); i++) {
+      System.out.print(b.charAt(i) + " ");
+    }
+    System.out.println();
   }
 
-  void assertValid(int i) {
-    assert i < length();
-    assert i >= 0;
+  static void printRow(int[][] k) {
+    if (!DEBUG)
+      return;
+    for (int i = 0; i < k[0].length; i++) {
+      System.out.print(k[1][i] + " ");
+    }
+    System.out.println();
   }
 
-  Slice slice(int start, int end) {
-    assert end <= str.length();
-    assert start >= 0;
-    return new Slice(str, this.start + start, this.start + end);
+  static void printf(String format, Object... args) {
+    if (DEBUG)
+      System.out.printf(format, args);
   }
 
-  Slice reverse() {
-    return new Slice(new StringBuilder(str).reverse().toString(), start, end);
-  }
-
-  char charAt(int i) {
-    return str.charAt(start + i);
-  }
-
-  int length() {
-    return end - start;
-  }
-
-  boolean contains(char c) {
-    return str.substring(start, end).contains(String.valueOf(c));
-  }
-
-  public String toString() {
-    return str.substring(start, end);
-  }
 }
 
 public class LCS {
 
   void copyLineUp(int[][] k) {
-    // System.out.println(Arrays.toString(k[0]));
     for (int i = 0; i < k[0].length; i++) {
       k[0][i] = k[1][i];
     }
   }
 
-  int[] ll(Slice a, Slice b) {
-    // System.out.printf("Finding ll for '%s' and '%s'\n", a, b);
-    // Basic LCS algorithm
+  int[] ll(String a, String b) {
+    // Debug.printf("Finding ll for '%s' and '%s'\n", a, b);
     int[][] k = new int[2][b.length() + 1];
+
+    Debug.printHeader(a, b);
+
     for (int posInA = 0; posInA < a.length(); posInA++) {
       copyLineUp(k);
       for (int posInB = 0; posInB < b.length(); posInB++) {
@@ -75,25 +63,23 @@ public class LCS {
           k[1][posInB + 1] = Integer.max(k[0][posInB + 1], k[1][posInB]);
         }
       }
+      Debug.printRow(k);
+
     }
-    // System.out.println(Arrays.toString(k[1]));
     return k[1];
   }
 
-  String lcs(Slice a, Slice b) {
-    // System.out.printf("Finding lcs for '%s' and '%s'\n", a, b);
+  String lcs(String a, String b) {
+    // Debug.printf("Finding lcs for '%s' and '%s'\n", a, b);
 
     if (b.length() == 0) {
-      // System.out.printf("LCS['%s', '%s']=%s \n", a.toString(), b.toString(), "");
       return "";
     }
     if (a.length() == 0) {
-      // System.out.printf("LCS['%s', '%s']=%s \n", a.toString(), b.toString(), "");
       return "";
     }
     if (a.length() == 1) {
-      // System.out.println("Single-char base case");
-      if (b.contains(a.charAt(0))) {
+      if (StringUtils.contains(b, a.charAt(0))) {
         return String.valueOf(a.charAt(0));
       } else
         return "";
@@ -101,8 +87,12 @@ public class LCS {
 
     int i = a.length() / 2;
 
-    int[] l1 = ll(a.slice(0, i), b);
-    int[] l2 = ll(a.reverse().slice(0, a.length() - i), b.reverse());
+    Debug.printf("\n\n\n");
+    Debug.printf("Split '%s'(a) into '%s':'%s'", a, a.substring(0, i),
+        StringUtils.reversed(a).substring(0, a.length() - i));
+
+    int[] l1 = ll(a.substring(0, i), b);
+    int[] l2 = ll(StringUtils.reversed(a).substring(0, a.length() - i), StringUtils.reversed(b));
 
     int max = Integer.MIN_VALUE;
     int maxIndex = 0;
@@ -113,21 +103,29 @@ public class LCS {
         maxIndex = j;
       }
     }
+    Debug.printf("Max LCS length is %d at index %d\n", max, maxIndex);
 
-    int k = maxIndex;
+    int splitBAt = maxIndex;
 
-    System.out.printf("\nMatching:\n '%s':'%s' --- %s\n'%s':'%s' --- %s\n",
-        a.slice(0, i),
-        b.slice(0, k),
-        Arrays.toString(l1),
-        a.slice(i, a.length()),
-        b.slice(k, b.length()),
-        Arrays.toString(l2));
+    Debug.printf("\n'%s':'%s'\n'%s':'%s'\n",
+        a.substring(0, i),
+        b.substring(0, splitBAt),
+        a.substring(i, a.length()),
+        b.substring(splitBAt, b.length()));
 
-    String c1 = lcs(a.slice(0, i), b.slice(0, k));
-    String c2 = lcs(a.slice(i, a.length()), b.slice(k, b.length()));
+    Debug.printf("\n\n\n");
+    String c1 = lcs(a.substring(0, i), b.substring(0, splitBAt));
+    String c2 = lcs(a.substring(i, a.length()), b.substring(splitBAt, b.length()));
 
-    System.out.printf("LCS['%s', '%s']=%s \n",
+    Debug.printf("\n\n'%s':'%s' ---> '%s'\n'%s':'%s' ---> '%s'\n",
+        a.substring(0, i),
+        b.substring(0, splitBAt),
+        c1,
+        a.substring(i, a.length()),
+        b.substring(splitBAt, b.length()),
+        c2);
+
+    Debug.printf("LCS['%s', '%s']=%s \n",
         a.toString(),
         b.toString(),
         c1 + c2);
@@ -136,9 +134,7 @@ public class LCS {
   }
 
   public String find(String A, String B) {
-    System.out.println(A);
-    System.out.println(B);
-    return lcs(new Slice(A), new Slice(B));
+    return lcs(A, B);
     // Implement the Hirschberg LCS algorithm
   }
 
